3ca420eb5a3f042c9fd9e9563f91de512c9f97bf
Cowington Post <cowingtonpost@gmail.com>
2023-01-21 09:46:25 -0800

    fixed OI subsystem

--- src/main/java/frc/robot/subsystems/Swimmy2023OISubsystem.java
+++ src/main/java/frc/robot/subsystems/Swimmy2023OISubsystem.java
@@ -3,9 +3,9 @@ package frc.robot.subsystems;
 import org.xero1425.base.subsystems.DriveBaseSubsystem;
 import org.xero1425.base.subsystems.Subsystem;
 
-public class OISubsystem extends org.xero1425.base.subsystems.oi.OISubsystem {
+public class Swimmy2023OISubsystem extends org.xero1425.base.subsystems.oi.OISubsystem {
 
-    public OISubsystem(Subsystem parent, DriveBaseSubsystem db) {
+    public Swimmy2023OISubsystem(Subsystem parent, DriveBaseSubsystem db) {
         super(parent, "swimmy2023oi", GamePadType.Swerve, db, true);
         //TODO Auto-generated constructor stub
     }
