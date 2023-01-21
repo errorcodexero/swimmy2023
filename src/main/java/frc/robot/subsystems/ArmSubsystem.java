0f23c69adb92a80c28b0b6f067d6592fd545556d
Cowington Post <cowingtonpost@gmail.com>
2023-01-21 11:39:56 -0800

    only initialize arm motors if simulation

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -3,19 +3,21 @@ package frc.robot.subsystems;
 import org.xero1425.base.subsystems.Subsystem;
 import org.xero1425.base.subsystems.motorsubsystem.MotorEncoderSubsystem;
 
+import edu.wpi.first.wpilibj.RobotBase;
+
 public class ArmSubsystem extends Subsystem {
     private MotorEncoderSubsystem motor_a_;
     private MotorEncoderSubsystem motor_b_;
 
     public ArmSubsystem(Subsystem parent) throws Exception {
         super(parent, "arm");
-
-        motor_a_ = new MotorEncoderSubsystem(this, "arm_motor_a", false);
-        addChild(motor_a_);
-        motor_b_ = new MotorEncoderSubsystem(this, "arm_motor_b", false);
-        addChild(motor_b_);
-
+        if (RobotBase.isSimulation()) {
+            motor_a_ = new MotorEncoderSubsystem(this, "arm_motor_a", false);
+            addChild(motor_a_);
+            motor_b_ = new MotorEncoderSubsystem(this, "motor_b", false);
+            addChild(motor_b_);
+        }
 
     }
-    
+
 }
