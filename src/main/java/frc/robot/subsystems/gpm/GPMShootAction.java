ac584f22a27c6d700e41067cd608af303a1dc3b2
Jack (Butch) Griffin <butchg@comcast.net>
2023-09-09 07:19:41 -0700

    added tenative shoot action that will need to be tuned for 3 cone automode

---
+++ src/main/java/frc/robot/subsystems/gpm/GPMShootAction.java
@@ -0,0 +1,51 @@
+package frc.robot.subsystems.gpm;
+
+import org.xero1425.base.actions.Action;
+
+import frc.robot.subsystems.arm.ArmStaggeredGotoMagicAction;
+import frc.robot.subsystems.grabber.GrabberShootAction;
+
+public class GPMShootAction extends Action {
+    private GPMSubsystem sub_ ;
+    private ArmStaggeredGotoMagicAction shoot_arm_action_ ;
+    private GrabberShootAction shoot_grabber_action_ ;
+    private boolean is_arm_in_place_ ;
+
+    public GPMShootAction(GPMSubsystem sub) throws Exception {
+        super(sub.getRobot().getMessageLogger());
+        
+        sub_ = sub ;
+        shoot_arm_action_ = new ArmStaggeredGotoMagicAction(sub_.getArm(), "shoot") ;
+        shoot_grabber_action_ = new GrabberShootAction(sub_.getGrabber());
+    }
+
+    @Override
+    public void start() throws Exception {
+        super.start() ;
+
+        is_arm_in_place_ = false ;
+        sub_.getArm().setAction(shoot_arm_action_, true) ;
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run() ;
+
+        if (!is_arm_in_place_) {
+            if (shoot_arm_action_.isDone()) {
+                sub_.getGrabber().setAction(shoot_grabber_action_, true);
+                is_arm_in_place_ = true ;
+            }
+        }
+        else {
+            if (shoot_arm_action_.isDone()) {
+                setDone() ;
+            }
+        }
+    }
+
+    @Override
+    public String toString(int indent) {
+        return spaces(indent) + "GPMStowAction";
+    }
+}
